15ba11842ef63d9b05d518e0b65e48a7ef222d5c
Лилия <l_loshakova123@mail.ru>
2022-07-20 20:39:31 +0300

    add solushion for task 0

--- ST0/Program.cs
+++ ST0/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Добро пожаловать, здесь вы сможете найти квадрат любого числа быстро и просто!)))");
+Console.WriteLine("Введите число:");
+string? inputLine = Console.ReadLine();
+if(inputLine != null)
+{
+    int inputNumber = int.Parse (inputLine);
+
+    ///int outNumber = inputNumber*inputNumber;
+    int outNumber = (int)Math.Pow(inputNumber,2);
+    Console.WriteLine("Квадрат числа:");
+    Console.WriteLine(outNumber);
+}
