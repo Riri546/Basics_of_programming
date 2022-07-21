d93e804ff6bd457ec470b5eccfb2b6afebdc4aac
Лилия <l_loshakova123@mail.ru>
2022-07-21 21:55:32 +0300

    Solution  task 8

---
+++ ST8/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+
+Console.Write("Enter an integer: ");
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null) 
+{
+    int inputNumber = int.Parse(inputLine);
+
+    if (inputNumber < 1) 
+    {
+       Console.Write("This number is less than 1, enter another number"); 
+    } 
+    else 
+    {
+        int B = 2;
+
+        string line = "";
+
+        while (B < inputNumber)
+        {
+            line = line + B + ", ";
+            B = B + 2;
+        }
+
+        Console.WriteLine("All even numbers from 1 to  " + inputNumber + ": ");
+
+        Console.WriteLine(line);
+    }
+
+}
