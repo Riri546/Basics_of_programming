d7c5b33ee1de11d84c274b5f7a945ae0b912506f
Лилия <l_loshakova123@mail.ru>
2022-07-21 20:33:04 +0300

    Solution task 4

--- ST4/Program.cs
+++ ST4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter a three-digit number:");
+
+// string inputLine = Console.ReadLine();
+        
+// if (inputLine != null)
+//  {
+//      int inputNumber = int.Parse(inputLine);
+
+//      int outputNumber = inputNumber % 10;
+
+//      Console.WriteLine(outputNumber);
+//  }
+
+ string? inputLineOne = Console.ReadLine();
+if(inputLineOne != null)
+{
+    string lastNum = inputLineOne.Substring(2,1);
+
+    Console.WriteLine(lastNum);
+}
