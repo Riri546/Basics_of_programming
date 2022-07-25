191d038e40df4694e99516279b88012e439a3774
Лилия <l_loshakova123@mail.ru>
2022-07-25 20:04:35 +0300

    Solution  task 9

---
+++ ST9/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Random numberSintezator = new Random();
+
+int number = numberSintezator.Next(10,100);
+
+Console.Write("This is a random number: ");
+Console.WriteLine(number);
+
+int firstNumber = number/10;
+
+int secondNumber = number%10;
+
+if (firstNumber>secondNumber)
+{
+    Console.Write("The largest digit of the number: ");
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.Write("The largest digit of the number: ");
+    Console.WriteLine (secondNumber); 
+}
