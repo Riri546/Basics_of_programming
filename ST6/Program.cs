cf1a73e5ca447992339d53bce01915f2300d6ba1
Лилия <l_loshakova123@mail.ru>
2022-07-25 19:16:15 +0300

    correction of an error in the task 6

--- ST6/Program.cs
+++ ST6/Program.cs
@@ -11,28 +11,31 @@ string? inputLineTwo = Console.ReadLine();
 Console.Write("Enter the third number: ");
 string? inputLineThree = Console.ReadLine();
 
-if (inputLineOne != null && inputLineTwo != null && inputLineThree != null) 
+   if (inputLineOne != null && inputLineTwo != null && inputLineThree != null)
 {
-    int A = int.Parse(inputLineOne);
-    int B = int.Parse(inputLineTwo);
-    int C = int.Parse(inputLineThree);
-
-    if (A > B)
-    {
+   int A = int.Parse(inputLineOne);
+   int B = int.Parse(inputLineTwo);
+   int C = int.Parse(inputLineThree);
+      if (A > B)
+   {
          if (A > C)
-         {
-            Console.Write("The first number is larger!");
-         }
-    } 
-    else 
-    {
-         if (B > C)
-         {
-            Console.Write("The second number is larger!");
-         }
-         else
-         {
-            Console.Write("The third number is larger!");
-         }
-    }
+      {
+         Console.Write("The first number is larger!");
+      }
+   else
+{
+   Console.Write("The third number is larger!");
+}
+   }
+   else
+   {  
+   if (B > C)
+   {
+   Console.Write("The second number is larger!");
+   }
+   else
+   {
+   Console.Write("The third number is larger!");
+   }
+   }
 }
