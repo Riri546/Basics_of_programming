4dbf60ef5321454e4b686e8716073223a75a4959
Лилия <l_loshakova123@mail.ru>
2022-07-26 22:19:55 +0300

    decoration solution  task 9

--- ST9/Program.cs
+++ ST9/Program.cs
@@ -1,21 +1,72 @@
-﻿System.Random numberSintezator = new Random();
+﻿//Problem solving method option 1
 
-int number = numberSintezator.Next(10,100);
-
-Console.Write("This is a random number: ");
-Console.WriteLine(number);
-
-int firstNumber = number/10;
-
-int secondNumber = number%10;
-
-if (firstNumber>secondNumber)
+void variant1(System.Random numberSintezator)
 {
-    Console.Write("The largest digit of the number: ");
-    Console.WriteLine(firstNumber);
+    int number = numberSintezator.Next(10,100);
+
+    Console.Write("This is a random number: ");
+    Console.WriteLine(number);
+
+    int firstNumber = number/10;
+
+    int secondNumber = number%10;
+
+    if (firstNumber>secondNumber)
+    {
+        Console.Write("The largest digit of the number: ");
+        Console.WriteLine(firstNumber);
+    }
+    else
+    {
+        Console.Write("The largest digit of the number: ");
+        Console.WriteLine (secondNumber); 
+    }
+
 }
-else
+
+//Problem solving method option 2
+void variant2(System.Random numberSintezator)
+
 {
-    Console.Write("The largest digit of the number: ");
-    Console.WriteLine (secondNumber); 
+    int numberLili = new Random().Next(10,100);
+
+    Console.Write("This is a random number: ");
+    Console.WriteLine(numberLili);
+
+    string stringNum = numberLili.ToString();
+
+    Console.Write("The biggest digit is: ");
+    if (stringNum[0] > stringNum[1])
+    {
+        Console.WriteLine(stringNum[0]);
+    }
+    else 
+    {
+     Console.WriteLine(stringNum[1]);   
+    }
 }
+
+//Problem solving method option 3
+void variant3(System.Random numberSintezator)
+
+{
+    char[] digits = numberSintezator.Next(10,100).ToString().ToArray();
+
+    firstNumber = ((int)digits[0])-48;
+    secondNumber = ((int)digits[1])-48;
+
+    int resultNumber = firstNumber>secondNumber?resultNumber=firstNumber:resultNumber=secondNumber;
+
+    Console.WriteLine(resultNumber);
+}
+
+
+
+
+System.Random numberSintezator = new Random();
+
+variant1(numberSintezator);
+
+variant2();
+
+variant3(numberSintezator);
