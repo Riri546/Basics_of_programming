fa8f8be342ee4cc78054ac78784a9be811a7fa88
Лилия <l_loshakova123@mail.ru>
2022-07-27 20:32:06 +0300

    Solution  task 10 (1)

--- ST10/Program.cs
+++ ST10/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿Console.Clear();
+
+//Problem solving method option 1
+
+void variant1(System.Random numberSintezator)
+
+{
+    int number = numberSintezator.Next(100,1000);
+
+        Console.Write("This is a random number: ");
+        Console.WriteLine(number);
+
+    int firstNumber = number / 100;
+
+    int secondNumber = number % 10;
+
+    int resultNumber = firstNumber * 10 + secondNumber;
+
+        Console.Write("Removed the second digit from a random number: ");
+        Console.WriteLine(resultNumber);
+}
+
+System.Random numberSintezator = new Random();
+
+variant1(numberSintezator);
