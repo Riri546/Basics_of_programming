ea2e625d342dd7f463549030ee15b43ea4a7fe67
Лилия <l_loshakova123@mail.ru>
2022-07-27 22:14:14 +0300

    Solution  task 12

--- ST12/Program.cs
+++ ST12/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Is your number a multiple of 7 and 23?");
+
+Console.Write("Enter number: ");
+string? firstNumber = Console.ReadLine();
+
+if (firstNumber!= null)
+{
+    int number= int.Parse(firstNumber);
+
+    int resultNumber1 = number%7;
+
+    if (resultNumber1 == 0)
+    {
+        Console.WriteLine("Yes, this number is a multiple of 7!");
+    }
+    else
+    {
+        Console.WriteLine("No , this number is not a multiple of 7: " + resultNumber1);
+    }
+
+    int resultNumber2 = number%23;
+
+    if (resultNumber2 == 0)
+    {
+        Console.WriteLine("Yes, this number is a multiple of 23!");
+    }
+    else
+    {
+        Console.WriteLine("No , this number is not a multiple of 23: " + resultNumber2);
+    }
+}
+
+
