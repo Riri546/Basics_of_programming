4bec38719976f489a4bee4d6b475d539b06c3918
Лилия <l_loshakova123@mail.ru>
2022-07-27 21:58:18 +0300

    Solution  task 11

--- ST11/Program.cs
+++ ST11/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿Console.Clear();
+
+Console.WriteLine("Is the second number a multiple of the first");
+
+Console.Write("Enter the first number: ");
+string? firstNumber = Console.ReadLine();
+
+Console.Write("Enter the second number: ");
+string? secondNumber = Console.ReadLine();
+
+if (firstNumber!= null && secondNumber != null)
+{
+    int first = int.Parse(firstNumber);
+    int second = int.Parse(secondNumber);
+
+    int result = second%first;
+
+    if (result == 0)
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No, remains: " + result);
+    }
+}
+
+
