c1c8480005f193c0cb302fe23f53b2f5e6b15a49
Лилия <l_loshakova123@mail.ru>
2022-07-29 22:25:38 +0300

    Solution  task 13

--- ST13/Program.cs
+++ ST13/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿Console.Clear();
+
+Console.WriteLine("Is one number the square of the second");
+
+Console.Write("Enter the first number: ");
+string? inputLineOne = Console.ReadLine();
+
+Console.Write("Enter the second number: ");
+string? inputLineTwo = Console.ReadLine();
+
+if ((inputLineOne != null) && (inputLineTwo != null))
+{
+    int inputNumOne = int.Parse(inputLineOne);
+    int inputNumTwo = int.Parse(inputLineTwo);
+
+    int a = inputNumOne * inputNumOne;
+    int b = inputNumTwo * inputNumTwo;
+
+    if((a == inputNumTwo) || (b == inputNumOne))
+    {
+        Console.WriteLine("Yes!");
+    }
+    else
+    {
+        Console.WriteLine("No!");
+    }
+}
