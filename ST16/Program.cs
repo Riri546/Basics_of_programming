086e99bd60604be40606bc8d406adf47911310c2
Лилия <l_loshakova123@mail.ru>
2022-07-29 15:14:29 +0300

    Solution  task 16

--- ST16/Program.cs
+++ ST16/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿Console.Clear();
+
+//Problem solving method option 1
+
+void variant1(System.Random numberSintezator)
+
+{
+    int number = numberSintezator.Next();
+
+        Console.Write("This is a random number: ");
+        Console.WriteLine(number);
+
+        int length = number.ToString().Length;
+
+        string str = number.ToString();
+
+        if (length > 3)
+        {
+            Console.Write("The third digit of a random number: ");
+            Console.WriteLine(str[2]);
+        }
+
+        else
+        {
+             Console.Write("Error: not enough numbers: ");
+        }
+}
+
+System.Random numberSintezator = new Random();
+
+variant1(numberSintezator);
