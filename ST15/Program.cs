d175e01f7c6160e48dd1c40186b0ad0aa9c8cd71
Лилия <l_loshakova123@mail.ru>
2022-07-29 09:44:48 +0300

    Solution  task 15

--- ST15/Program.cs
+++ ST15/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+//
+
+void variant1(System.Random numberSintezator)
+
+{
+    int number = numberSintezator.Next(100,1000);
+
+        Console.Write("This is a random number: ");
+        Console.WriteLine(number);
+
+    int firstNumber= number % 100;
+
+    int resultNumber = firstNumber / 10;
+
+        Console.Write("The second digit of the random number: ");
+        Console.WriteLine(resultNumber);
+}
+
+System.Random numberSintezator = new Random();
+
+variant1(numberSintezator);
