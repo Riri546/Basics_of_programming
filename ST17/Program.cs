be658d394b0da88ef5574d7c8e9ffb0d47bf2a29
Лилия <l_loshakova123@mail.ru>
2022-07-31 22:29:47 +0300

    Sketch

--- ST17/Program.cs
+++ ST17/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Метод считывает точки и возвращает массив с ним
+int [,] readPoint()
+{
+    string inputLine = Console.ReadLine();
+
+    string coordX = inputLine.Substring(0,inputLine.IndexOf(";"));
+    coordX = coordX.Substring(coordX.IndexOf("=")+1);
+
+    string coordY= inputLine.Substring(inputLine.IndexOf(";")+1);
+    coordY = coordY.Substring(coordY.IndexOf("=")+1);
+
+    Console.WriteLine(coordX+" "+coordY);
+
+    return new int [1,1];
+
+}
+
+//Печатает номер четверти
+//void ptintQuter(int[,] arrayPoint)
+//{
+
+//}
+
+//int[,] arrayPoint = readPoint();
+
+//printQuter(arrayPoint);
+
+//printQuter (readPoint());
+
