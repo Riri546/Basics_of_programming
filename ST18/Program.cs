94f49d15014e49bdd894812d7c6929283d750b60
Лилия <l_loshakova123@mail.ru>
2022-08-02 18:56:15 +0300

    task design 18

--- ST18/Program.cs
+++ ST18/Program.cs
@@ -2,17 +2,17 @@
 
 void printAnswer(int number)
 {
-    if(number == 1) Console.WriteLine("Acceptable: x>0, y>0");
-    if(number == 2) Console.WriteLine("Acceptable: x<0, y>0");
-    if(number == 3) Console.WriteLine("Acceptable: x<0, y<0");
-    if(number == 4) Console.WriteLine("Acceptable: x>0, y<0");
+    if (number == 1) Console.WriteLine("Acceptable: x>0, y>0");
+    if (number == 2) Console.WriteLine("Acceptable: x<0, y>0");
+    if (number == 3) Console.WriteLine("Acceptable: x<0, y<0");
+    if (number == 4) Console.WriteLine("Acceptable: x>0, y<0");
 }
 
 string? inputLine = Console.ReadLine();
 
-if(inputLine != null)
+if (inputLine != null)
 {
-    int inputNumber = int.Parse (inputLine);
+    int inputNumber = int.Parse(inputLine);
 
     printAnswer(inputNumber);
 }
