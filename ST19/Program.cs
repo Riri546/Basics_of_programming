76df33c70dd6980e89b18d32aeff77f81976d017
Лилия <l_loshakova123@mail.ru>
2022-08-02 18:59:24 +0300

    Task design 19

--- ST19/Program.cs
+++ ST19/Program.cs
@@ -8,26 +8,26 @@ int coordYPointB;
 double lengthAB;
 
 
-//Считывает координаты точек A и B
+//Reads coordinates of points A and B
 void readDataOfPoint()
 {
-    Console.WriteLine("Введите координату X точки A");
+    Console.WriteLine("Enter the X coordinate of point A");
     coordXPointA = int.Parse(Console.ReadLine());
 
-    Console.WriteLine("Введите координату Y точки A");
+    Console.WriteLine("Enter the Y coordinate of point A");
     coordYPointA = int.Parse(Console.ReadLine());
 
-    Console.WriteLine("Введите координату X точки B");
+    Console.WriteLine("Enter the X coordinate of point B");
     coordXPointB = int.Parse(Console.ReadLine());
 
-    Console.WriteLine("Введите координату Y точки B");
+    Console.WriteLine("Enter the Y coordinate of point B");
     coordYPointB = int.Parse(Console.ReadLine());
 }
 
-//вычисляет растояние между точками A и B
+//Calculates the distance between points A and B
 void conculateLateLengthAB()
 {
-    lengthAB = Math.Sqrt(Math.Pow((coordXPointA - coordXPointB),2) + Math.Pow ((coordYPointA - coordYPointB),2));
+    lengthAB = Math.Sqrt(Math.Pow((coordXPointA - coordXPointB), 2) + Math.Pow((coordYPointA - coordYPointB), 2));
 }
 
 readDataOfPoint();
