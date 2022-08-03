16ac593f1e57714288c0efe745ad699bde92304d
Лилия <l_loshakova123@mail.ru>
2022-08-03 22:38:19 +0300

    Correction of an error in task 20

---
+++ ST20/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.Write("Enter number: ");
+
+string? inputLineN = Console.ReadLine();
+
+if (inputLineN != null)
+{
+    int numberN = int.Parse(inputLineN);
+
+    string lineN = " ";
+    string lineNN = string.Empty;
+    int s = 1;
+
+    while (s <= numberN)
+    {
+        lineN = lineN + s + "  ";
+        lineNN = lineNN + (s * s).ToString() + "  ";
+        s++;
+    }
+
+    Console.WriteLine(lineN);
+    Console.WriteLine(lineNN);
+}
