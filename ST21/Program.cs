34d6673d5803922185753ff3ffac2c3bff5e97e6
Лилия <l_loshakova123@mail.ru>
2022-08-05 20:37:48 +0300

    Solusion task 21

--- ST21/Program.cs
+++ ST21/Program.cs
@@ -1,5 +1,38 @@
 ﻿Console.Clear();
-int number = ConcurrentExclus
 
-string result = String.Format("{0:d}", number);
-Console.WriteLine(result); 
+int P;
+
+int D1;
+int D2;
+int D3;
+int D4;
+int D5;
+
+//
+void enterNumber()
+{
+    Console.Write("Enter Number: ");
+    P = int.Parse(Console.ReadLine());
+}
+//Decomposition of a number
+void decompositionOfaNumber()
+{
+    D1 = (P/10000);
+    D2 = (P/1000)%10;
+    D3 = (P/100)%10;
+    D4 = (P/10)%10;
+    D5 = P%10;
+}
+
+
+enterNumber();
+decompositionOfaNumber();
+
+    if (D1 == D5 && D2 == D4)
+    {
+        Console.Write("Yes");
+    }
+    else
+    {
+        Console.Write("No");
+    }
