b668db2eb13e70b5ec9bd3808fa303b36a097552
Лилия <l_loshakova123@mail.ru>
2022-08-06 23:24:07 +0300

    Solusion task 26

--- ST26/Program.cs
+++ ST26/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.Clear();
+
+Console.Write("Enter number: ");
+string inputLine = Console.ReadLine() ?? "";
+int inputNum = int.Parse(inputLine);
+
+Console.WriteLine(sumNums(inputNum));
+
+int sumNums(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= inputNum; i++)
+    {
+        //sum = sum * i;
+        sum *= i;
+    }
+    return sum;
+}
