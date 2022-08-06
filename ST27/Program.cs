b810958f05e3b2196b4463e986852dc8687585cd
Лилия <l_loshakova123@mail.ru>
2022-08-07 00:05:56 +0300

    Solusion task 27

--- ST27/Program.cs
+++ ST27/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.Clear();
+
+System.Random numberSintezator = new Random();
+
+void VariantNaive()
+{
+    int i = 0;
+    while (i < 8)
+    {
+        Console.Write(numberSintezator.Next(0, 2));
+        i++;
+    }
+}
+
+
+VariantNaive();
