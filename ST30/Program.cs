0c78f45d02200e4939d4001aabe7238bbe4952e2
Лилия <l_loshakova123@mail.ru>
2022-08-07 21:12:21 +0300

    Adding time to task 30

--- ST30/Program.cs
+++ ST30/Program.cs
@@ -3,18 +3,23 @@
 Console.Clear();
 
 System.Random numberSintezator = new Random();
+int t;
 
 void VariantNaive()
 {
     int i = 0;
-    Console.Write("[");
+    Console.Write("Solution: [");
     while (i < 8 - 1)
     {
         Console.Write(numberSintezator.Next(0,100) + ",");
         i++;
     }
     Console.Write(numberSintezator.Next(0,100));
-    Console.Write("]");
+    Console.WriteLine("]");
+   
+    t = Environment.TickCount;
+    Console.WriteLine("Solution time: {0} ms", Environment.TickCount - t);
+
 }
 
 
