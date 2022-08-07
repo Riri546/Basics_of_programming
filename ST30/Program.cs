06c07b7328527b8ac34d0fdbdaf4580b7ada752d
Лилия <l_loshakova123@mail.ru>
2022-08-07 21:01:34 +0300

    Solusion task 30

--- ST30/Program.cs
+++ ST30/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿//Write a program that sets an array of 8 elements and displays them on the screen.
+
+Console.Clear();
+
+System.Random numberSintezator = new Random();
+
+void VariantNaive()
+{
+    int i = 0;
+    Console.Write("[");
+    while (i < 8 - 1)
+    {
+        Console.Write(numberSintezator.Next(0,100) + ",");
+        i++;
+    }
+    Console.Write(numberSintezator.Next(0,100));
+    Console.Write("]");
+}
+
+
+VariantNaive();
