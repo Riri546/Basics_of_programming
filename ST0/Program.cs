e499f1d6933df4737d667d8c24653521fc106aad
Лилия <l_loshakova123@mail.ru>
2022-08-08 12:00:12 +0300

    Improving the solution of task 0 using methods and time

--- ST0/Program.cs
+++ ST0/Program.cs
@@ -1,14 +1,37 @@
-﻿Console.Clear();
+﻿//Write a program that takes a number as input and outputs its square (a number multiplied by itself).
 
-Console.WriteLine("Welcome, here you can find the square of any number quickly and easily!)))");
-Console.Write("Enter a number: ");
-string? inputLine = Console.ReadLine();
-if(inputLine != null)
+Console.Clear();
+
+int inputNumber;
+int outNumber;
+int t;
+
+DateTime timePoint = DateTime.Now;
+
+//The method takes a number from the console
+void ReadNumber()
 {
-    int inputNumber = int.Parse (inputLine);
-
-    ///int outNumber = inputNumber*inputNumber;
-    int outNumber = (int)Math.Pow(inputNumber,2);
-    Console.WriteLine("The square of the number:");
-    Console.WriteLine(outNumber);
+    Console.WriteLine("Welcome, here you can find the square of any number quickly and easily!)))");
+    Console.Write("Enter a number: ");
+    string inputLine = Console.ReadLine() ?? "";
+    inputNumber = int.Parse (inputLine);
 }
+
+//The method squares the number
+void ConculateSimple()
+{
+    ///int outNumber = inputNumber*inputNumber;
+    outNumber = (int)Math.Pow(inputNumber,2);
+}
+
+//The method outputs the result of the solution
+void PrintResult()
+{
+    t = Environment.TickCount;
+    Console.WriteLine("The square of the number: " + outNumber);
+    Console.WriteLine("Solution time: {0} ms", Environment.TickCount - t);
+}
+
+ReadNumber();
+ConculateSimple();
+PrintResult();
