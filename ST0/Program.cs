40fd4bb706da144e3e84690d18f22d22d1f56d5b
Лилия <l_loshakova123@mail.ru>
2022-08-08 15:15:36 +0300

    Improving the solution of task 1 using methods and time

--- ST0/Program.cs
+++ ST0/Program.cs
@@ -14,14 +14,14 @@ void ReadNumber()
     Console.WriteLine("Welcome, here you can find the square of any number quickly and easily!)))");
     Console.Write("Enter a number: ");
     string inputLine = Console.ReadLine() ?? "";
-    inputNumber = int.Parse (inputLine);
+    inputNumber = int.Parse(inputLine);
 }
 
 //The method squares the number
 void ConculateSimple()
 {
     ///int outNumber = inputNumber*inputNumber;
-    outNumber = (int)Math.Pow(inputNumber,2);
+    outNumber = (int)Math.Pow(inputNumber, 2);
 }
 
 //The method outputs the result of the solution
--- ST1/Program.cs
+++ ST1/Program.cs
@@ -1,29 +1,59 @@
-﻿Console.Clear();
+﻿//Write a program that takes two numbers as input and checks whether the first number is the square of the second.
 
+Console.Clear();
+
+int inputNumberOne;
+int inputNumberTwo;
+int t;
+
+//The method takes a number from the console
+void ReadNumber()
+{
 Console.WriteLine("Let's check if the first number is the square of the second one?))");
 
 Console.Write("Enter the first number: ");
-string? inputLineOne = Console.ReadLine();
+string inputLineOne = Console.ReadLine() ?? "";
+inputNumberOne = int.Parse(inputLineOne);
 
 Console.Write("Enter the second number: ");
-string? inputLineTwo = Console.ReadLine();
+string inputLineTwo = Console.ReadLine() ?? "";
+inputNumberTwo = int.Parse(inputLineTwo);
+}
 
-if(inputLineOne != null && inputLineTwo != null){
-    int inputNumberOne = int.Parse(inputLineOne);
-    int inputNumberTwo = int.Parse(inputLineTwo);
-    
-    //if (inputNumberOne == inputNumberTwo * inputNumberTwo){
-    //    Console.WriteLine("Yes");
-    //}
-    //else
-    //{
-    //Console.WriteLine("No");
-    //}
-    if (inputNumberTwo == Math.Sqrt(inputNumberOne)){
-        Console.WriteLine("Constitute");
+//The method solves in a simple way
+void ConculateSimple()
+{
+    if (inputNumberOne == inputNumberTwo * inputNumberTwo)
+    {
+        t = Environment.TickCount;
+        Console.WriteLine("Simple solution: Constitute");
+        Console.WriteLine("Simple solution time: {0} ms", Environment.TickCount - t);
     }
     else
     {
-        Console.WriteLine("Is not");
+        t = Environment.TickCount;
+        Console.WriteLine("Simple solution: Is not");
+        Console.WriteLine("Simple solution time: {0} ms", Environment.TickCount - t);
     }
 }
+
+//The method solves using Math
+void ConculateMath()
+{
+    if (inputNumberTwo == Math.Sqrt(inputNumberOne))
+    {
+        t = Environment.TickCount;
+        Console.WriteLine("Math solution: Constitute");
+        Console.WriteLine("Math solution time: {0} ms", Environment.TickCount - t);
+    }
+    else
+    {
+        t = Environment.TickCount;
+        Console.WriteLine("Math solution: Is not");
+        Console.WriteLine("Math solution time: {0} ms", Environment.TickCount - t);
+    }
+}
+
+ReadNumber();
+ConculateSimple();
+ConculateMath();
