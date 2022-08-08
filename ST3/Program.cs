a09e2cda1fe1838d5d6a8369efbdc4ab399c46b2
Лилия <l_loshakova123@mail.ru>
2022-08-08 15:46:20 +0300

    Improving the solution of task 3 using methods and time

--- ST3/Program.cs
+++ ST3/Program.cs
@@ -1,25 +1,45 @@
-﻿Console.Clear();
+﻿//Write a program that takes one number (N) as input, and shows all integers in the range from -N to N as output.
 
-Console.Write("Enter an integer: ");
+Console.Clear();
 
-string? inputLineOne = Console.ReadLine();
+int inputNumber;
+int startNumber;
+string lineOutput;
 
-if(inputLineOne != null)
+int t;
+
+//The method takes a number from the console
+void ReadNumber()
 {
-    int inputNumber = int.Parse(inputLineOne);
-
-    int startNumber = 1;
-
-    string lineOutput = "0";
-
-    while (startNumber <= inputNumber)
-    {
-        lineOutput = startNumber*(-1)+", "+lineOutput+", "+startNumber;
-        startNumber++;
-    }
-        
-//  lineOutput = lineOutput+inputNumber; 
-
-    Console.WriteLine(lineOutput);
+    Console.Write("Enter an integer: ");
+    string inputLineOne = Console.ReadLine() ?? "";
+    inputNumber = int.Parse(inputLineOne);
 }
-  
+
+//The method solves in a simple way
+void Conculate()
+{
+    startNumber = 1;
+
+    lineOutput = "0";
+
+        while (startNumber <= inputNumber)
+        {
+            lineOutput = startNumber*(-1)+", "+lineOutput+", "+startNumber;
+            startNumber++;
+        }
+            
+    //  lineOutput = lineOutput+inputNumber;
+}
+
+//The method outputs the result of the solution
+void PrintResult()
+{
+    t = Environment.TickCount;
+    Console.WriteLine(lineOutput);
+    Console.WriteLine("Solution time: {0} ms", Environment.TickCount - t);
+}
+
+ReadNumber();
+Conculate();
+PrintResult();
