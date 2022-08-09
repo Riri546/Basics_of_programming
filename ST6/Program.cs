b04c6e65ba805ec0cfe16b4d1640aca25e646ab7
Лилия <l_loshakova123@mail.ru>
2022-08-09 16:15:46 +0300

    Improving the solution of task 6 using methods and time

--- ST6/Program.cs
+++ ST6/Program.cs
@@ -2,42 +2,65 @@
 
 Console.Clear();
 
-Console.WriteLine("Enter two numbers and I'll tell you more");
+int A;
+int B;
+int C;
 
-Console.Write("Enter the first number: ");
-string? inputLineOne = Console.ReadLine();
+int t;
 
-Console.Write("Enter the second number: ");
-string? inputLineTwo = Console.ReadLine();
-
-Console.Write("Enter the third number: ");
-string? inputLineThree = Console.ReadLine();
-
-   if (inputLineOne != null && inputLineTwo != null && inputLineThree != null)
+//The method takes a number from the console
+void ReadNumber()
 {
-   int A = int.Parse(inputLineOne);
-   int B = int.Parse(inputLineTwo);
-   int C = int.Parse(inputLineThree);
-      if (A > B)
-   {
-         if (A > C)
-      {
-         Console.Write("The first number is larger!");
-      }
-   else
+    Console.WriteLine("Enter two numbers and I'll tell you more");
+
+    Console.Write("Enter the first number: ");
+    string inputLineOne = Console.ReadLine() ?? "";
+    A = int.Parse(inputLineOne);
+
+    Console.Write("Enter the second number: ");
+    string inputLineTwo = Console.ReadLine() ?? "";
+    B = int.Parse(inputLineTwo);
+
+
+    Console.Write("Enter the third number: ");
+    string inputLineThree = Console.ReadLine() ?? "";
+    C = int.Parse(inputLineThree);
+}
+
+//Solving the problem
+void ConculateSimple()
 {
-   Console.Write("The third number is larger!");
+    if (A > B)
+    {
+        if (A > C)
+        {
+            t = Environment.TickCount;
+            Console.WriteLine("The first number is larger!");
+            Console.WriteLine("Solution time: {0} ms", Environment.TickCount - t);
+        }
+        else
+        {
+            t = Environment.TickCount;
+            Console.WriteLine("The third number is larger!");
+            Console.WriteLine("Solution time: {0} ms", Environment.TickCount - t);
+        }
+    }
+    else
+    {
+        if (B > C)
+        {
+            t = Environment.TickCount;
+            Console.WriteLine("The second number is larger!");
+            Console.WriteLine("Solution time: {0} ms", Environment.TickCount - t);
+        }
+        else
+        {
+            t = Environment.TickCount;
+            Console.WriteLine("The third number is larger!");
+            Console.WriteLine("Solution time: {0} ms", Environment.TickCount - t);
+        }
+    }
 }
-   }
-   else
-   {  
-   if (B > C)
-   {
-   Console.Write("The second number is larger!");
-   }
-   else
-   {
-   Console.Write("The third number is larger!");
-   }
-   }
-}
+
+ReadNumber();
+ConculateSimple();
