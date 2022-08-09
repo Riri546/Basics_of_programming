7952c78b894098d5ff3ca0945f9b10ce4a562f8b
Лилия <l_loshakova123@mail.ru>
2022-08-09 16:27:35 +0300

    Improving the solution of task 8 using methods and time

--- ST8/Program.cs
+++ ST8/Program.cs
@@ -2,34 +2,50 @@
 
 Console.Clear();
 
-Console.Write("Enter an integer: ");
-string? inputLine = Console.ReadLine();
+int inputNumber;
+int B;
+string line;
 
-if (inputLine != null)
+int t;
+
+//The method takes a number from the console
+void ReadNumber()
 {
-    int inputNumber = int.Parse(inputLine);
+    Console.Write("Enter an integer: ");
+    string inputLine = Console.ReadLine() ?? "";
+    inputNumber = int.Parse(inputLine);
+}
 
+//Solving the problem
+void ConculateSimple()
+{
     if (inputNumber < 1)
     {
+        t = Environment.TickCount;
         Console.Write("This number is less than 1, enter another number");
+        Console.WriteLine("Solution time: {0} ms", Environment.TickCount - t);
     }
     else
     {
-        int B = 2;
-        
-        string line = "";
-        
+        B = 2;
+
+        line = "";
+
         while (B <= inputNumber - 2)
         {
-            
+
             line = line + B + ", ";
-            
+
             B = B + 2;
         }
-            line = line + B;
-
+        line = line + B;
+        t = Environment.TickCount;
         Console.WriteLine("All even numbers from 1 to " + inputNumber + ": ");
 
         Console.WriteLine(line);
+        Console.WriteLine("Solution time: {0} ms", Environment.TickCount - t);
     }
 }
+
+ReadNumber();
+ConculateSimple();
