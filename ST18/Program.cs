dae0535cbb2139a4e2f1394d0275a03cab29be3a
Лилия <l_loshakova123@mail.ru>
2022-08-12 00:59:52 +0300

    I returned it to its original form, I did not like the work of the code with the correction (ST18)

--- ST18/Program.cs
+++ ST18/Program.cs
@@ -3,21 +3,19 @@
 
 Console.Clear();
 
-Console.Write("Enter number: ");
-string inputLine = Console.ReadLine() ?? "";
-int inputNumber = int.Parse(inputLine);
-
-//The method outputs the result to the console 
 void PrintAnswer(int number)
 {
-
     if (number == 1) Console.WriteLine("Acceptable: x>0, y>0");
     if (number == 2) Console.WriteLine("Acceptable: x<0, y>0");
     if (number == 3) Console.WriteLine("Acceptable: x<0, y<0");
     if (number == 4) Console.WriteLine("Acceptable: x>0, y<0");
 }
 
-Console.ReadLine();
+string? inputLine = Console.ReadLine();
 
-PrintAnswer(inputNumber);
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
 
+    PrintAnswer(inputNumber);
+}
