f1b6c3db998d7515c1a8a0f16eb698c5565e0b4a
Лилия <l_loshakova123@mail.ru>
2022-08-11 20:17:32 +0300

    Solusion task 31(2)

--- ST31/Program.cs
+++ ST31/Program.cs
@@ -55,6 +55,7 @@ void PrintResult()
     Console.WriteLine("Variant naive time: {0} ms", Environment.TickCount - t);
 }
 
+//The method outputs a random array of numbers to the console
 void PrintIntArray(int[] inputArray)
 {
     i = 0;
--- ST32/Program.cs
+++ ST32/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿//Write a program replacing array elements: 
+//replace the positive elements with the corresponding negative ones, and vice versa.
+
+Console.Clear();
+
+//The method returns an array filled with random numbers from -9 to 9.
+int[] FillingArray()
+{
+    int[] outArray = new int[12];
+    int i = 0;
+    System.Random numberSintezator = new Random();
+
+    while (i < 12)
+    {
+        outArray[i] = numberSintezator.Next(-9, 10);
+        i++;
+    }
+    return outArray;
+}
+
+void PrintIntArray(int[] inputArray)
+{
+    i = 0;
+
+    while (i < inputArray.Length - 1)
+    {
+        Console.Write(inputArray[i] + ", ");
+        i++;
+    }
+    Console.WriteLine(inputArray[i]);
+}
