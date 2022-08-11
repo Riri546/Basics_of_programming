95c0116d9bf8b7ae0d33d859ba93812c7b9f9248
Лилия <l_loshakova123@mail.ru>
2022-08-11 12:59:04 +0300

    Improving the solution of task 14 using methods and time

--- ST14/Program.cs
+++ ST14/Program.cs
@@ -1,21 +1,44 @@
-﻿Console.Clear();
+﻿//Write a program that accepts a three-digit number as input and shows the second digit of this number as output
+
+Console.Clear();
 
 //Problem solving method option 1
 
 void Variant1(System.Random numberSintezator)
 
 {
-    int number = numberSintezator.Next(100,1000);
+    int number;
+    int firstNumber;
+    int resultNumber;
+
+    //The method outputs a random number
+    void RandomNumber()
+    {
+        number = numberSintezator.Next(100, 1000);
 
         Console.Write("This is a random number: ");
         Console.WriteLine(number);
 
-    int firstNumber = number % 100;
+    }
 
-    int resultNumber = firstNumber / 10;
+    //Solving the problem
+    void ConculateSimple()
+    {
+        firstNumber = number % 100;
 
+        resultNumber = firstNumber / 10;
+    }
+
+    //The method outputs the result of the solution
+    void PrintResult()
+    {
         Console.Write("The second digit of the random number: ");
         Console.WriteLine(resultNumber);
+    }
+
+    RandomNumber();
+    ConculateSimple();
+    PrintResult();
 }
 
 System.Random numberSintezator = new Random();
