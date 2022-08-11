c5bfe606e7491f01e1962165ae14ca4534cd8bcb
Лилия <l_loshakova123@mail.ru>
2022-08-11 13:14:49 +0300

    Adding a comment about a task 17

--- ST17/Program.cs
+++ ST17/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Clear();
+﻿//Write a program that takes the coordinates of a point (X and Y) as input, with X ≠ 0 and Y ≠ 0 
+//and outputs the number of the quarter of the plane in which this point is located.
+
+Console.Clear();
 
 //The method reads the points and returns an array with it
 Console.Write("Enter a number: ");
