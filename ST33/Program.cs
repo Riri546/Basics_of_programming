a1f16ba7aca5e562500d82e0dc2fe985eb1863bf
Лилия <l_loshakova123@mail.ru>
2022-08-11 20:51:26 +0300

    Solution of task 32

--- ST33/Program.cs
+++ ST33/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿//Set the array. Write a program that determines whether a given number is present in an array.
+
+Console.Clear();
+
+//The method returns an array filled with random numbers from -9 to 9.
+int[] FillingArray()
+{
+    int[] outArray = new int[12];
+    i = 0;
+    System.Random numberSintezator = new Random();
+
+    while (i < 12)
+    {
+        outArray[i] = numberSintezator.Next(-9, 10);
+        i++;
+    }
+    return outArray;
+}
+
+//The method deals with replacing positive numbers with negative ones and vice versa
+int[] ColculateTask(int[] inputArray)
+{
+    int[] outputArray = new int[12];
+    int i = 0;
+    while (i < 12)
+    {
+        outputArray[i] = inputArray[i] * (-1);
+        i++;
+    }
+    return outputArray;
+}
+
+//The method outputs a random array of numbers to the console
+void PrintIntArray(int[] inputArray)
+{
+    i = 0;
+
+    while (i < inputArray.Length - 1)
+    {
+        Console.Write(inputArray[i] + ", ");
+        i++;
+    }
+    Console.WriteLine(inputArray[i]);
+}
