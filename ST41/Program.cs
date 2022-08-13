855bab4790eece5d9a29b5ec7af7973fabeb385d
Лилия <l_loshakova123@mail.ru>
2022-08-14 00:44:05 +0300

    Solution of task 41

--- ST41/Program.cs
+++ ST41/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Write a program that will convert a decimal number to binary.
+
+Console.Clear();
+
+Console.Write("Enter a number in binary calculus: ");
+
+int t = Environment.TickCount;
+Console.WriteLine("A number in binary calculus: " + Convert.ToString(int.Parse(Console.ReadLine() ?? ""), 2));
+Console.WriteLine("Time to solve the problem: {0} ms", Environment.TickCount - t);
+
+
+        
