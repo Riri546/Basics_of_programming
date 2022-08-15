98b5d26545e429965280e30074ab697e33447514
Лилия <l_loshakova123@mail.ru>
2022-08-15 18:30:26 +0300

    Adding comments to methods and time for solving in task 43

--- ST44/Program.cs
+++ ST44/Program.cs
@@ -2,68 +2,61 @@
 
 Console.Clear();
 
-int i;
 int m = 0;
+int[] array = new int[m];
 
 //The method takes a number from the console
-int[] ReadNumber()
+int[] ReadNumber(int[] array)
 {
-    Console.Write("Enter a number: ");
-    int[] array = new int[m];
+    Console.Write("Enter the numbers (separated by a space): ");
 
-    for (i = 0; i < m; i++)
+    for (int i = 0; i < m; i++)
     {
         array[i] = int.Parse(Console.ReadLine() ?? "");
     }
+
     return array;
-}
 
-//The method outputs a random array of numbers to the console
-void PrintIntArray(int[] array)
-{
-    i = 0;
-
-    while (i < array.Length - 1)
-    {
-        Console.Write(array[i] + ", ");
-        i++;
-    }
-    Console.WriteLine(array[i]);
 }
 
 //Solution task 44
 int ConculateTask(int[] array)
 {
-    int resultNumber = 0;
-    i = 0;
+    int count = 0;
+    int i = 0;
 
     for (i = 0; i < m; i++)
     {
         if (array[i] > 0)
         {
-            resultNumber++;
+            count++;
         }
     }
-    return resultNumber;
+    return count;
 }
 
 //The method outputs the result of the solution
-void PrintResult(int resultNumber)
+void PrintResult(int count)
 {
     DateTime d1 = DateTime.Now;
-    Console.WriteLine("The entered numbers are greater than 0: " + resultNumber);
-    Console.Write("Solution time: ");
+    Console.WriteLine("The entered numbers are greater than 0: " + count);
+    Console.WriteLine("Solution time: ");
     Console.WriteLine(DateTime.Now - d1);
 }
 
-
 int[] testArray = ReadNumber();
-PrintIntArray(testArray);
 
-int resultNumber = ConculateTask(testArray);
-PrintResult(resultNumber);
+ConculateTask(testArray);
+Console.WriteLine(PrintResult());
 
 
 
 //The user enters the number of clicks, then the program monitors 
 //the clicks and outputs how many numbers greater than 0 were entered.
+
+
+
+// Console.Write("Введите элементы(через пробел): ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = arr.Count(x => x > 0);
+// Console.WriteLine($"Кол-во элементов > 0: {count}");
