89ce5f07c9a6557a848b0e32a02e98265492ba05
Лилия <l_loshakova123@mail.ru>
2022-08-17 09:49:16 +0300

    Solution task 44 without *

--- ST44/Program.cs
+++ ST44/Program.cs
@@ -2,53 +2,15 @@
 
 Console.Clear();
 
-int m = 0;
-int[] array = new int[m];
-
-//The method takes a number from the console
-int[] ReadNumber(int[] array)
+//Solution task 44(with the int type, you can enter 4 digits, with long 8)
+void ConculateTask()
 {
-    Console.Write("Enter the numbers (separated by a space): ");
-
-    for (int i = 0; i < m; i++)
-    {
-        array[i] = int.Parse(Console.ReadLine() ?? "");
-    }
-
-    return array;
-
+    Console.Write("Enter the elements (separated by a space): ");
+    long[] inputArray = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+    long count = inputArray.Count(x => x > 0);
+    Console.WriteLine($"Number of elements > 0: {count}");
 }
-
-//Solution task 44
-int ConculateTask(int[] array)
-{
-    int count = 0;
-    int i = 0;
-
-    for (i = 0; i < m; i++)
-    {
-        if (array[i] > 0)
-        {
-            count++;
-        }
-    }
-    return count;
-}
-
-//The method outputs the result of the solution
-void PrintResult(int count)
-{
-    DateTime d1 = DateTime.Now;
-    Console.WriteLine("The entered numbers are greater than 0: " + count);
-    Console.WriteLine("Solution time: ");
-    Console.WriteLine(DateTime.Now - d1);
-}
-
-int[] testArray = ReadNumber();
-
-ConculateTask(testArray);
-Console.WriteLine(PrintResult());
-
+ConculateTask();
 
 
 //The user enters the number of clicks, then the program monitors 
@@ -56,7 +18,3 @@ Console.WriteLine(PrintResult());
 
 
 
-// Console.Write("Введите элементы(через пробел): ");
-// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-// int count = arr.Count(x => x > 0);
-// Console.WriteLine($"Кол-во элементов > 0: {count}");
