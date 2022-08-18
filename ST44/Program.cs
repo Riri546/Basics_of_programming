44e1c731da3f22f79158ec78b82f4504de4dbc1f
Лилия <l_loshakova123@mail.ru>
2022-08-19 02:03:22 +0300

    The task 50  was solved without *, I started sketching the task with*

--- ST44/Program.cs
+++ ST44/Program.cs
@@ -5,16 +5,25 @@ Console.Clear();
 //Solution task 44(with the int type, you can enter 4 digits, with long 8)
 void ConculateTask()
 {
+    //Outputs text to the console
     Console.Write("Enter the elements (separated by a space): ");
+
+    //Array.ConvertAll - Converts an array of one type to an array of another type.
+    //.Split - The String.Split method creates an array of substrings by splitting the input string
+    // by one or more delimiters. This method is often the simplest 
+    //a way to divide a string by word boundaries. It is also used to split strings
+    // by other specific characters or strings.
+
     long[] inputArray = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+
+    //Checking for "> 0"
     long count = inputArray.Count(x => x > 0);
+
+    //Outputs the result to the console
     Console.WriteLine($"Number of elements > 0: {count}");
 }
 ConculateTask();
 
-
-//The user enters the number of clicks, then the program monitors 
-//the clicks and outputs how many numbers greater than 0 were entered.
 
 
 
--- ST50/Program.cs
+++ ST50/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Specify a two-dimensional array of size m×n filled with random real numbers.
+
+// Console.Clear();
+
+// double[,] a = new double[5, 10];
+// Random random = new Random();
+// for (int i = 0; i < 5; i++)
+// {
+//     for (int j = 0; j < 10; j++)
+//     {
+//         a[i, j] = random.NextDouble() * 100; // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1
+//         Console.Write("{0,6:F2}", a[i, j]);
+//     }
+//     Console.WriteLine();
+// }
+
+//Method for filling a two-dimensional array
+double[,] FillTowDimArray(int countRow, int countColumn)
+{
+    System.Random numberSyntezator = new System.Random();
+
+    double[,] outArray = new double[countRow, countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            outArray[i, j] = numberSyntezator.NextDouble() * 100; // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1
+            Console.Write("{0,6:F2}", outArray[i, j]);
+        }
+        Console.WriteLine();
+    }
+    return outArray;
+}
+
+
+//The method prints an array in yellow
+ void PrintColorTwoDimArray(double[,] inpujitArray)
+{
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Yellow;
+
+    for (int i = 0; i < inpujitArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inpujitArray.GetLength(1); j++)
+        {
+            Console.Write("{0,6:F2}", inpujitArray[i, j]);
+        }
+        Console.ResetColor();
+    }
+    
+}
+double[,] twoDimArray = FillTowDimArray(3, 4);
+PrintColorTwoDimArray(twoDimArray);
+
+
