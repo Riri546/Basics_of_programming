32db1aa16290bdf67169db86e513f4a0902ac298
Лилия <l_loshakova123@mail.ru>
2022-08-19 17:04:12 +0300

    Solution of task 50

--- ST50/Program.cs
+++ ST50/Program.cs
@@ -25,31 +25,37 @@ double[,] FillTowDimArray(int countRow, int countColumn)
         for (int j = 0; j < countColumn; j++)
         {
             outArray[i, j] = numberSyntezator.NextDouble() * 100; // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1
-            Console.Write("{0,6:F2}", outArray[i, j]);
+            // Console.Write("{0,6:F2}", outArray[i, j]);
         }
-        Console.WriteLine();
+        // Console.WriteLine();
     }
     return outArray;
 }
 
+//Array of colors for the task with *
+ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black, ConsoleColor.Blue, ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue, ConsoleColor.DarkCyan, ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen, ConsoleColor.DarkMagenta, ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow, ConsoleColor.Gray, ConsoleColor.Green,
+                                        ConsoleColor.Magenta, ConsoleColor.Red, ConsoleColor.White, 
+                                        ConsoleColor.Yellow};
 
-//The method prints an array in yellow
- void PrintColorTwoDimArray(double[,] inpujitArray)
+//A method that prints the solution in a multi-colored format
+ void PrintColorTwoDimArray(double[,] outArray)
 {
-    Console.WriteLine();
-    Console.ForegroundColor = ConsoleColor.Yellow;
-
-    for (int i = 0; i < inpujitArray.GetLength(0); i++)
+    for (int i = 0; i < outArray.GetLength(0); i++)
     {
-        for (int j = 0; j < inpujitArray.GetLength(1); j++)
+        for (int j = 0; j < outArray.GetLength(1); j++)
         {
-            Console.Write("{0,6:F2}", inpujitArray[i, j]);
+            Console.ForegroundColor = col[new System.Random().Next(0,16)];
+            Console.Write("{0,6:F2}", outArray[i, j]);
+            Console.ResetColor();
         }
-        Console.ResetColor();
+        Console.WriteLine();
     }
-    
 }
-double[,] twoDimArray = FillTowDimArray(3, 4);
+
+double[,] twoDimArray = FillTowDimArray(5, 8);
 PrintColorTwoDimArray(twoDimArray);
 
 
