2e481b2a1b7da74939bfdad14c3422d63c5e6837
Лилия <l_loshakova123@mail.ru>
2022-08-20 16:23:09 +0300

    New folder ST45

--- ST45/Program.cs
+++ ST45/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Write a program that finds the intersection point of two straight lines given by the equations 
+//y = k1 * x + b1, y = k2 * x + b2; the values b1, k1, b2 and k2 are set by the user.
+
+Console.Clear();
+
+double b1;
+double k1;
+double b2;
+double k2;
+double x;
+double y;
+
+//The method accepts input data from the user
+void ReadLine()
+{
+    Console.Write("Enter the b1: ");
+    b1 = int.Parse(Console.ReadLine() ?? "");
+
+    Console.Write("Enter the k1: ");
+    k1 = int.Parse(Console.ReadLine() ?? "");
+
+    Console.Write("Enter the b2: ");
+    b2 = int.Parse(Console.ReadLine() ?? "");
+
+    Console.Write("Enter the k2: ");
+    k2 = int.Parse(Console.ReadLine() ?? "");
+}
+
+//The method solves the task 45
+void ConculateTask()
+{
+    x = ((b2 - b1) / (k1 - k2));
+    y = k1 * ((b2 - b1) / (k1 - k2)) + b1;
+
+
+}
+
+//The method prints the result
+void PrintSolution()
+{
+    Console.WriteLine("( x= " + x + "; y= " + y + ")");
+}
+
+ReadLine();
+ConculateTask();
+PrintSolution();
