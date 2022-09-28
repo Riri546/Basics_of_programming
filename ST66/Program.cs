d5a6f68c397bc43d5b5ff7647abaf35e387594b2
Лилия <l_loshakova123@mail.ru>
2022-09-29 00:18:41 +0300

    Solution task 66

--- ST66/Program.cs
+++ ST66/Program.cs
@@ -6,7 +6,7 @@ Console.Clear();
 int numberA = ReadData("Set the number A: ");
 int numberB = ReadData("Set the number B: ");
 
-Console.Write(PowNumber(numberA, numberB));
+
 
 int ReadData(string line)
 {
@@ -14,8 +14,56 @@ int ReadData(string line)
     return int.Parse(Console.ReadLine() ?? "");
 }
 
+int MyPow(int a, int b)
+{
+    if (b == 2)
+    {
+        return a * a;
+    }
+    if (b == 1)
+    {
+        return a;
+    }
+    if (b % 2 == 0)
+    {
+        return MyPow(a, b / 2) * MyPow(a, b / 2);
+    }
+    else
+    {
+        return MyPow(a, b / 2) * MyPow(a, (b / 2) + 1);
+    }
+}
+
 int PowNumber(int a, int b)
 {
     if (b == 1) return a;
     return a * PowNumber(a, --b);
 }
+
+//The body of the task, launches the ce methods
+void Result()
+{
+    DateTime d1 = DateTime.Now;
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("Solution PowNumber: ");
+    Console.WriteLine(PowNumber(numberA, numberB));
+    Console.ResetColor();
+
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Write("Solution time: ");
+    Console.ResetColor();
+    Console.WriteLine(DateTime.Now - d1);
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("Solution MyPow: ");
+    Console.WriteLine(MyPow(numberA, numberB));
+    Console.ResetColor();
+
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Write("Solution time: ");
+    Console.ResetColor();
+    Console.WriteLine(DateTime.Now - d1);
+}
+
+Result();
