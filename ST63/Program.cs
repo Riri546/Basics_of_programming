32cdbf789b6b593ac17344f5b6fe17c271dbb7a3
Лилия <l_loshakova123@mail.ru>
2022-09-28 23:16:49 +0300

    Solution task 63

--- ST63/Program.cs
+++ ST63/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Set the value to N.
+// Write a program that outputs all natural numbers in the range from 1 to N.
+
+Console.Clear();
+
+int ReadData()
+{
+    Console.WriteLine("Задайте число: ");
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+int NuturalNumberPrinter(int num)
+{
+    if (num == 2) return 1;
+    {
+        num--;
+        Console.WriteLine(num);
+        Console.WriteLine(NuturalNumberPrinter(num) + " ");
+    }
+    return num;
+}
+ int inputNumber = ReadData();
+ NuturalNumberPrinter(inputNumber);
