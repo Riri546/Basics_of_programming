26af9f1974af8239ecda1691b0290a08d4415011
Лилия <l_loshakova123@mail.ru>
2022-09-28 23:54:56 +0300

    Solution task 65

--- ST66/Program.cs
+++ ST66/Program.cs
@@ -1,3 +1,21 @@
 ﻿//Write a program that takes two numbers A and B as input, 
 //and raises the number to an integer power of B using recursion.
 
+Console.Clear();
+
+int numberA = ReadData("Set the number A: ");
+int numberB = ReadData("Set the number B: ");
+
+Console.Write(PowNumber(numberA, numberB));
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+int PowNumber(int a, int b)
+{
+    if (b == 1) return a;
+    return a * PowNumber(a, --b);
+}
