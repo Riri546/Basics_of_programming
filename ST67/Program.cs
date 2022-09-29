c4710c699e0530c98230d928e2b75da672e7f99f
Лилия <l_loshakova123@mail.ru>
2022-09-29 16:21:55 +0300

    test

--- ST67/Program.cs
+++ ST67/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//
+Console.Clear();
+
+int numberM = ReadData("Set the number M: ");
+int numberN = ReadData("Set the number N: ");
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+void NuturalNumberPrinter(int m, int n)
+{
+    if (m - 1 == n) return;
+    {
+        Console.Write(m + " ");
+        NuturalNumberPrinter(m + 1, n);
+    }
+}
+
+NuturalNumberPrinter(numberM, numberN);
