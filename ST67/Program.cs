ac59a9a94a4ba96c84dbb99554356053969b881d
Лилия <l_loshakova123@mail.ru>
2022-09-30 19:13:06 +0300

    Solution Task 67

--- ST67/Program.cs
+++ ST67/Program.cs
@@ -1,36 +1,38 @@
-﻿//
+﻿//Set the values of M and N. Write a program that outputs all natural numbers in the range from M to N.
 Console.Clear();
 
 int numberM = ReadData("Set the number M: ");
 int numberN = ReadData("Set the number N: ");
 
+//Variable input method
 int ReadData(string line)
 {
+    Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine(line);
+    Console.ResetColor();
     return int.Parse(Console.ReadLine() ?? "");
 }
 
+//Recursive calculation method
 void NuturalNumberPrinter(int m, int n)
 {
-    if (n == m +1 ) return;
+    if (m - 1 == n) return;
     {
         Console.Write(m + " ");
         NuturalNumberPrinter(m + 1, n);
     }
 }
 
-NuturalNumberPrinter(numberM, numberN);
-
-int Sum(int m, int n)
+//The body of the task, launches the ce methods
+void Result()
 {
-    int sum = 0;
+    DateTime d1 = DateTime.Now;
 
-    if (m - 1 == n) return 1;
-    {
-        sum += m;
-        Console.Write(m + " ");
-        Sum(m + 1, n);
-    }
-    Console.WriteLine(sum);
-    return sum;
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Write("Answer: ");
+    Console.ResetColor();
+    NuturalNumberPrinter(numberM, numberN);
 }
+
+Result();
+
