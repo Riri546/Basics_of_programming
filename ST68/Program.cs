077061ef0c76bd7d8a47e8c12e13b9d7693c08db
Лилия <l_loshakova123@mail.ru>
2022-10-01 11:32:39 +0300

    Solution Task 68

--- ST68/Program.cs
+++ ST68/Program.cs
@@ -5,28 +5,40 @@ Console.Clear();
 int numberM = ReadData("Set the number M: ");
 int numberN = ReadData("Set the number N: ");
 
+//Variable input method
 int ReadData(string line)
 {
     Console.WriteLine(line);
     return int.Parse(Console.ReadLine() ?? "");
 }
 
-
-// функция сумма чисел от M до N
-int SumMN(int m, int n)
+//method of finding the sum of numbers from M to N by the formula: ∑= (N 2– М 2+ М+ N)/ 2
+static int SumNumbers(int M, int N)
 {
-    int res = m;
-    if (m == n)
-        return 0;
-    else
-    {
-        m++;
-        res = m + SumMN(m, n);
-        return res;
-    }
+    if (M == 0) return (N * (N + 1)) / 2;            // If M is zero
+    else if (N == 0) return (M * (M + 1)) / 2;       // If N is zero
+    else if (M == N) return M;                       // If M=N
+    else if (M < N) return N + SumNumbers(M, N - 1); // If M<N
+    else return N + SumNumbers(M, N + 1);            // If M>N
 }
 
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.Write("Answer: ");
-Console.ResetColor();
-SumMN(numberM, numberN);
+//The body of the task, launches the ce methods
+void Result()
+{
+    DateTime d1 = DateTime.Now;
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("Result, sum numbers = ");
+    Console.ResetColor();
+    Console.WriteLine(SumNumbers(numberM, numberN));
+
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Write("Solution time: ");
+    Console.ResetColor();
+    Console.WriteLine(DateTime.Now - d1);
+}
+
+Result();
+
+
+
